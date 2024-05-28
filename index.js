bc446efa10e541f740705c9ac5367bf023465c33
JoWabz <ioana019@yahoo.com>
2024-05-28 21:50:08 +0300

    etape3-4extra

--- index.js
+++ index.js
@@ -143,6 +143,8 @@
 const express = require("express");
 const fs= require('fs');
 
+const path=require('path');
+
 app= express();
 console.log("Folder proiect", __dirname);
 console.log("Cale fisier", __filename);
@@ -152,8 +154,28 @@ app.set("view engine","ejs");
 
 app.use("/resurse", express.static(__dirname+"/resurse"));
 
+
+
 app.get("/", function(req, res){
-    res.send("<b style='color:red'>Buna </b> ziua!");
+    // res.sendFile(path.join(__dirname,"index.html"));
+    res.render("pagini/index");
 })
+
+// app.get("/pagina", function(req, res){
+//     res.send("<b style='color:red'>alceva </b> ziua!");
+// })
+
+
+app.get("/vector/:a", function(req, res){
+    let vec = [1,10,100,1000];
+    res.write(vec[req.params.a]+" ");
+    res.write(vec[req.params.a]*2+" ");
+    res.end();
+})
+
+app.get("/*", function(req, res){
+    res.send("Nu stiu")
+})
+
 app.listen(8080);
 console.log("Serverul a pornit");
